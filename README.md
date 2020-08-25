edfbedb8c24e61e27120433306dd9ce19d882a39
Erdi <Erdi.kidane@engineering.ucla.edu>
2020-08-25 05:07:56 -0700

    added multi processing downloads more than 1 file at a time

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ format must be:
 URL NAME
 where URL = some youtube video
 where NAME = name of the song
+
+Some kinks to work out for later:
+*need to validate input better
+*limit on the number of possible processes spawned maybe? (more research into parallel programming in python needed)
--- you2MP3.py
+++ you2MP3.py
@@ -1,13 +1,18 @@
 from __future__ import unicode_literals
 import youtube_dl
+import multiprocessing
+from multiprocessing import Process, freeze_support
+import os
 
-f = open("songList.txt", "r")
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
+
+f = open("C:/Users/Erdi/Desktop/Projects/python/testProjects/songList.txt", "r")
 arry = []
 counter = 0
 SAVE_PATH = '/Downloads/'
-for x in f:
-    arry.append(x.split())
-
+def convert(counter):
     ydl_opts = {
         'format':'bestaudio/best',
         'extractaudio':True,
@@ -23,7 +28,38 @@ for x in f:
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([arry[counter][0]])
-    counter+=1
+    #counter+=1
+for x in f:
+    arry.append(x.split())
+# for x in f:
+#     arry.append(x.split())
 
+#     ydl_opts = {
+#         'format':'bestaudio/best',
+#         'extractaudio':True,
+#         'audioformat':'mp3',
+#         'outtmpl':SAVE_PATH + arry[counter][1] +'.%(ext)s',
+#         'noplaylist':True,
+#         'nocheckcertificate':True,
+#         'postprocessors': [{
+#             'key': 'FFmpegExtractAudio',
+#             'preferredcodec': 'mp3',
+#             'preferredquality': '192',
+#         }],
+#     }
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         ydl.download([arry[counter][0]])
+#     counter+=1
+#convert(0)
+processes = []
+for i in range(len(arry)):#support for parallel programming 
+    if __name__ == '__main__':
+        freeze_support()
+        p = multiprocessing.Process(target=convert, args=[i])
+        p.start()
+        processes.append(p)
+    #counter+=1
+for process in processes:
+    process.join()
 
 f.close()
